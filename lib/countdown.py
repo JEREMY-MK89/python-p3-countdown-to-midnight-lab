85b6d145147d73e5fd2a9e78d9d57e72ac50b029
moses <moses.kingi@moringaschool.com>
2024-01-13 17:18:19 +0300

    while loops and conditions lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,26 @@
 # your code goes here!
+import time
+import sys
+
+def countdown(number=10):
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        sys.stdout.flush()  # Flush the output buffer
+
+    print("HAPPY NEW YEAR!")
+
+countdown()
+
+
+
+def countdown_with_sleep(number=10):
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        sys.stdout.flush()
+        time.sleep(1)  # Pause for one second
+
+    print("HAPPY NEW YEAR!")
+
+    
